50116cfd13b7949440d96ab26c82314c51ac2bdf
jean <jeanclaverie@outlook.com>
2025-01-19 23:07:56 +0100

    init : back next js with auth and user module

---
+++ back/src/user/users.service.ts
@@ -0,0 +1,57 @@
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import User from './user.entity';
+import { CreateUserDto } from './dto/user.dto';
+
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async getAllUsers() {
+    const users = this.usersRepository.find();
+    return users;
+  }
+
+  async getUserById(id: number) {
+    const user = await this.usersRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+    throw new NotFoundException('Could not find the user');
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingUser) {
+      throw new BadRequestException('User with this email already exists');
+    }
+
+    const newUser = this.usersRepository.create(createUserDto);
+    await this.usersRepository.save(newUser);
+    return newUser;
+  }
+
+  async deleteById(id: number) {
+    const user = await this.usersRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!user) {
+      return null;
+    }
+
+    await this.usersRepository.remove(user);
+    return user;
+  }
+}
